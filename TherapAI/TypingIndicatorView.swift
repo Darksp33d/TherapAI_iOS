af23b125cf9cc356a9dbc57b23ceb92a96a77316
Darksp33d <147221220+Darksp33d@users.noreply.github.com>
2023-10-13 09:31:13 -0400

    Update TypingIndicatorView.swift

--- TherapAI/TypingIndicatorView.swift
+++ TherapAI/TypingIndicatorView.swift
@@ -10,7 +10,7 @@ struct TypingIndicatorView: View {
                     .frame(width: 6, height: 6)
                     .foregroundColor(Color.gray)
                     .scaleEffect(self.scaleValues[index])
-                    .modifier(FadeInModifier(delay: 0.4))  // Apply the fade-in effect with a delay
+                    .modifier(FadeInModifier(delay: 0.4)) 
                     .onAppear {
                         withAnimation(Animation.easeInOut(duration: 0.5).repeatForever().delay(Double(index) * 0.2)) {
                             self.scaleValues[index] = 1.4
